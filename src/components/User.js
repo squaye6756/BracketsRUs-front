c44b8ed776b67d357bb55c90cb132bf3ce26527d
Samuel Quaye <quayes955@gmail.com>
2021-11-18 16:39:19 -0500

    logged in user can create a tournament

--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,22 @@
 import {useState} from 'react';
+import axios from 'axios';
+import MakeTourney from './MakeTourney.js';
 
 const User = ({currUser}) => {
+
+    const handleCreateTourney = (newTourney) => {
+        console.log(newTourney);
+        axios.post('https://bracketsrus.herokuapp.com/api/tournaments', newTourney)
+        .then((response) => {
+            console.log(response);
+        });
+    }
 
     return (
         <>
             <h2>Welcome, {currUser.username}</h2>
+            <MakeTourney handleCreateTourney={handleCreateTourney}
+            currUser={currUser}/>
         </>
     )
 }
