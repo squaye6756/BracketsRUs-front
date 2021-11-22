7de06ed69b91275f6a0ffd24316d01813b3b7159
Samuel Quaye <quayes955@gmail.com>
2021-11-21 22:45:07 -0500

    css for owned and participating tournaments

--- src/components/Participation.js
+++ src/components/Participation.js
@@ -1,23 +1,24 @@
 import {Link} from 'react-router-dom';
+import '../views/participating.css';
 
 const Participation = ({currUser, tournaments}) => {
     return (
         <>
             {currUser.username &&
               <>
+                <h1>Participating in:</h1>
+                <hr/>
                 <div className='tourneyList'>
-                  <h1>Participating in:</h1>
-                  <hr/>
                   {tournaments.map((tourney) => {
                     return(
                       <>
                         {tourney.players.includes(currUser.id) &&
                           <div className='participating-single-tourney' key={`participating-${tourney.id}`}>
-                              <h2>{tourney.name}</h2>
-                              <h3>{tourney.details}</h3>
-                              <h3>{tourney.game}</h3>
-                              <h3>{tourney.prizes}</h3>
-                              <h3>{tourney.limit}</h3>
+                              <h2>Name: {tourney.name}</h2>
+                              <h3>Game: {tourney.game}</h3>
+                              <h3>Details: {tourney.details}</h3>
+                              <h3>Prize: {tourney.prizes}</h3>
+                              <h3>Limit: {tourney.limit}</h3>
                               <Link to={`/tournament/${tourney.id}`}>See Details</Link>
                           </div>
                         }
